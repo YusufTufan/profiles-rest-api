9ae5ffbe00ad00d83e2c6c17894a108c6e33e2e5
Yusuf Tufan <yusufftufan@gamil.com>
2025-08-26 23:08:50 +0300

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8m%lasnr)7j90+3xb^z4!xc#t8hfz1cgc97n(+to9-4p02ubet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -127,3 +127,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
+
+STATIC_ROOT = 'static/'
